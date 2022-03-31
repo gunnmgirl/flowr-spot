9e8d95e14aa8815f60c1d2971ec1fd3e0afbd043
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2022-03-31 13:13:35 +0200

    FS Adds mobile navigation view (#17)
    Co-authored-by: Dzana <mop@L-113.local>

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Flex, HStack, Text, useDisclosure, Button } from "@chakra-ui/react";
+import { Flex, Stack, Text, useDisclosure, Button } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import { useWindowSize } from "react-use";
 
 import useStore from "../store";
 
@@ -8,9 +9,11 @@ import Signup from "../features/auth/components/signup";
 import Logo from "./logo";
 import Login from "../features/auth/components/login";
 import UserAvatar from "./user-avatar";
+import NavigationMobile from "./navigation-mobile";
 
 const Navigation = () => {
   const isAuth = useStore((state) => state.isAuth);
+  const { width } = useWindowSize();
   const {
     isOpen: isSignupOpen,
     onOpen: onSignupOpen,
@@ -26,26 +29,43 @@ const Navigation = () => {
   return (
     <Flex px="28px" py="20px" justify="space-between">
       <Logo />
-      <HStack fontSize="sm" spacing="54px">
-        <NavLink
-          to="/"
-          style={({ isActive }) => (isActive ? { color: "purple" } : undefined)}
-        >
-          Flowers
-        </NavLink>
-        <NavLink
-          to="/sightings"
-          style={({ isActive }) => (isActive ? { color: "purple" } : undefined)}
-          data-cy="sightings-button"
-        >
-          Latest Sightings
-        </NavLink>
-        <NavLink
-          to="/favorites"
-          style={({ isActive }) => (isActive ? { color: "purple" } : undefined)}
-        >
-          Favorites
-        </NavLink>
+      <Stack
+        align="center"
+        direction={["column", "row"]}
+        fontSize="sm"
+        spacing="54px"
+      >
+        {width < 500 ? (
+          <NavigationMobile />
+        ) : (
+          <>
+            <NavLink
+              to="/"
+              style={({ isActive }) =>
+                isActive ? { color: "purple" } : undefined
+              }
+            >
+              Flowers
+            </NavLink>
+            <NavLink
+              to="/sightings"
+              style={({ isActive }) =>
+                isActive ? { color: "purple" } : undefined
+              }
+              data-cy="sightings-button"
+            >
+              Latest Sightings
+            </NavLink>
+            <NavLink
+              to="/favorites"
+              style={({ isActive }) =>
+                isActive ? { color: "purple" } : undefined
+              }
+            >
+              Favorites
+            </NavLink>
+          </>
+        )}
         {isAuth ? (
           <UserAvatar />
         ) : (
@@ -69,7 +89,7 @@ const Navigation = () => {
             </Button>
           </>
         )}
-      </HStack>
+      </Stack>
       <Signup
         onSignupSuccess={onLoginOpen}
         isOpen={isSignupOpen}
